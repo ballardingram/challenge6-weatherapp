8fe5ea6ba66bc4ac84550c5f7b5987f22ced6980
Ballard Ingram <ballardingram@outlook.com>
2022-03-22 11:34:00 -0500

    css, html, js - added bootstrap, and time code in java

--- assets/js/script.js
+++ assets/js/script.js
@@ -12,3 +12,28 @@ $(document).ready(function(){
 	  // Else use ‘day’ theme
 	  document.body.className = "day";
 });
+
+// Important default API key and city information.
+var key = '26ddf8f43bcf591b20c4ad83cf52357c'
+var city = 'Chicago'
+
+// This code is used to asses the current date and time.
+var date = moment().format('dddd, MMMM Do YYYY');
+var dateTime = moment().format('YYYY-MM-DD HH:MM:SS')
+
+// This code generates the list of cities for local storage from Search and save it for the array.
+var cityHist = [];
+$('.search').on("click", function (event) {
+    event.preventDefault();
+    city = $(this).parent('.btnPar').siblings('.textVal').val().trim();
+    if (city === "") {
+        return;
+    };
+    cityHist.push(city)
+    localStorage.setItem('city', JSON.stringify(cityHist));
+    fiveForecastEl.empty();
+    getHistory();
+    getWeatherToday();
+});
+
+// This code is going to create additional buttons once the search option is selected.
